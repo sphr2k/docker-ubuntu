be39695ee7f6166bb53c5f11bcdad796c2f1c713
sphr2k <mail@janwerner.de>
2024-05-22 14:59:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-# Generate a 20-character random password
-USERNAME=web2tty
-PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c 20)
-
-# Output the generated password
-echo "web2tty username: $USERNAME"
-echo "web2tty password: $PASSWORD"
-
-# Start tty2web with the generated password
-/usr/local/bin/tty2web --port 8080 -c $USERNAME:$PASSWORD -w /bin/bash
+if [ "$#" -gt 0 ]; then
+  exec /usr/local/bin/tty2web "$@"
+else
+  # Default username & generate password
+  DEFAULT_USERNAME=web2tty
+  DEFAULT_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c 20)
+  
+  # Output the generated password
+  echo "web2tty username: $DEFAULT_USERNAME"
+  echo "web2tty password: $DEFAULT_PASSWORD"
+  exec /usr/local/bin/tty2web --port 8080 -c $DEFAULT_USERNAME:$DEFAULT_PASSWORD -w /bin/bash
+fi
